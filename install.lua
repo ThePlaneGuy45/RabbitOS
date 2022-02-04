ceca857fb02ce099d8291d733d25e18356e60493
ThePlaneGuy45 <69761838+ThePlaneGuy45@users.noreply.github.com>
2022-02-03 21:46:11 -0500

    Update install.lua

--- install.lua
+++ install.lua
@@ -1,1 +1,30 @@
-print("Installing RabbitOS");
+print("Installing RabbitOS v1.0")
+term.setTextColor(colors.red)
+print("WARNING: Installing RabbitOS will delete ALL you files, do you want to procede? Y/N")
+while(true) do
+  local event, key, isHeld = os.pullEvent("key")
+  if isHeld then
+    if key == keys.y then
+      print("Removing Files")
+      local files = fs.list("/")
+      for k,v in ipairs(files) do
+        if v ~= "rom" and v ~= shell.getRunningProgram() then
+          print("-"..v)
+          fs.delete(v)
+          sleep(.1)
+        end
+      end
+      print("Deleted Files")
+      sleep(.2)
+      print("Installing Files")
+      fs.makeDir("/apps/")
+      fs.makeDir("/system/")
+      fs.makeDir("/user/")
+      break
+    else
+      print("Terminating Installation")
+      sleep(.4)
+      print("Installation Terminated")
+      break
+  end
+end
