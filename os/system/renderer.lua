51ad6f5fd2fd5c9c1d03e15b5bc5b16f25f69e4e
ThePlaneGuy45 <69761838+ThePlaneGuy45@users.noreply.github.com>
2022-02-04 20:57:19 -0500

    Update renderer.lua

--- os/system/renderer.lua
+++ os/system/renderer.lua
@@ -1,1 +1,14 @@
--- This is a renderer, im still working on it :P
+-- Screen Data
+local w, h = term.getSize()
+local imgFile = fs.open("/system/screen/screen.img", "r")
+local imgData = imgFile.readAll()
+local screen = window.create(term.current(), 1, 1, w, h)
+-- Terminal Setup
+screen.setBackgroundColor(colors.black)
+screen.setTextColor(colors.white)
+screen.clear()
+screen.setCursorPos(1,1)
+-- Render Screen
+--while(true) do
+--  local x, y = 0, 0
+--end
